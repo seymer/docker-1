9864fcda6df0455ebe50a5bfdb8164ef3f1da0a4
maycon <maycon@localhost.localdomain>
2017-08-23 21:55:54 -0300

    Creating Dinamic Variables to Define Options of Each Environment
    Created variables and template files. Change run.sh files to delete
    and create a new docker-compose.yml file from the respective
    template.yml.

--- run-bio.sh
+++ run-bio.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+source variables.env;rm -rf docker-compose-bio.yml; envsubst < "template-bio.yml" > "docker-compose-bio.yml";
 docker-compose stop \
 	&& docker-compose -f docker-compose-bio.yml build \
 	&& docker-compose -f docker-compose-bio.yml up &
