b938f1216aee3df76ffa9c695393388a7ecb40dd
Maycon Brito <maycon.brito@bioextratus.rede.bioextratus>
2017-10-31 13:06:53 -0200

    Merging Bio and Home

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+source variables.env;rm -rf docker-compose.yml; envsubst < "template.yml" > "docker-compose.yml";
 docker-compose stop \
 	&& docker-compose build \
 	&& docker-compose up &
