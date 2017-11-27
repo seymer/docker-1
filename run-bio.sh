9685952a6b3358bd7c517c0cf073362eb4b7a7d6
Maycon Brito <maycon.brito@bioextratus.rede.bioextratus>
2017-11-27 16:27:33 -0200

    Created Docker Compose Only for MySQL
    Changed Docker Compose BIO to use PHP 7 edition to Bio, Mysql, Nginx and
    Composer.
    
    Later, we'll unify and paremetrize the run.sh file.

--- run-bio.sh
+++ run-bio.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-source variables.env;rm -rf docker-compose-bio.yml; envsubst < "template-bio.yml" > "docker-compose-bio.yml";
 docker-compose stop \
 	&& docker-compose -f docker-compose-bio.yml build \
 	&& docker-compose -f docker-compose-bio.yml up &
--- run.sh
+++ run.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-source variables.env;rm -rf docker-compose.yml; envsubst < "template.yml" > "docker-compose.yml";
 docker-compose stop \
 	&& docker-compose build \
 	&& docker-compose up &
